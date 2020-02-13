4826eb371066f7474a34a38f3e1c0ccab28b876d
yy2905 <60762215+yy2905@users.noreply.github.com>
2020-02-12 23:20:08 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 $('.d-list-item').on('click', function() {
 	var dCoverUrl = $(this).attr('data-destination-pic')
   console.log(dCoverUrl)
+  // this will empty div with id d-pics
   $('#d-pics').empty()
+  // this will show a background image on the screen
   $('#d-pics').css('background-image', `url(${dCoverUrl})`)
 })
